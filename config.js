356b0d578df389143f1ddc35c3eb0efc63a37f5a
videovolna <videovolna@gmail.com>
2018-03-18 03:00:11 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,316 @@
+var productRotationData_1363309381731 = {
+    "hotspots": null,
+    "images": [
+        {
+            "src": "images/artdenim00001.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00001.JPG"
+        },
+        {
+            "src": "images/artdenim00002.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00002.JPG"
+        },
+        {
+            "src": "images/artdenim00003.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00003.JPG"
+        },
+        {
+            "src": "images/artdenim00004.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00004.JPG"
+        },
+        {
+            "src": "images/artdenim00005.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00005.JPG"
+        },
+        {
+            "src": "images/artdenim00006.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00006.JPG"
+        },
+        {
+            "src": "images/artdenim00007.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00007.JPG"
+        },
+        {
+            "src": "images/artdenim00008.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00008.JPG"
+        },
+        {
+            "src": "images/artdenim00009.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00009.JPG"
+        },
+        {
+            "src": "images/artdenim00010.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00010.JPG"
+        },
+        {
+            "src": "images/artdenim00011.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00011.JPG"
+        },
+        {
+            "src": "images/artdenim00012.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00012.JPG"
+        },
+        {
+            "src": "images/artdenim00013.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00013.JPG"
+        },
+        {
+            "src": "images/artdenim00014.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00014.JPG"
+        },
+        {
+            "src": "images/artdenim00015.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00015.JPG"
+        },
+        {
+            "src": "images/artdenim00016.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00016.JPG"
+        },
+        {
+            "src": "images/artdenim00017.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00017.JPG"
+        },
+        {
+            "src": "images/artdenim00018.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00018.JPG"
+        },
+        {
+            "src": "images/artdenim00019.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00019.JPG"
+        },
+        {
+            "src": "images/artdenim00020.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00020.JPG"
+        },
+        {
+            "src": "images/artdenim00021.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00021.JPG"
+        },
+        {
+            "src": "images/artdenim00022.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00022.JPG"
+        },
+        {
+            "src": "images/artdenim00023.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00023.JPG"
+        },
+        {
+            "src": "images/artdenim00024.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00024.JPG"
+        },
+        {
+            "src": "images/artdenim00025.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00025.JPG"
+        },
+        {
+            "src": "images/artdenim00026.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00026.JPG"
+        },
+        {
+            "src": "images/artdenim00027.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00027.JPG"
+        },
+        {
+            "src": "images/artdenim00028.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00028.JPG"
+        },
+        {
+            "src": "images/artdenim00029.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00029.JPG"
+        },
+        {
+            "src": "images/artdenim00030.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00030.JPG"
+        },
+        {
+            "src": "images/artdenim00031.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00031.JPG"
+        },
+        {
+            "src": "images/artdenim00032.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00032.JPG"
+        },
+        {
+            "src": "images/artdenim00033.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00033.JPG"
+        },
+        {
+            "src": "images/artdenim00034.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00034.JPG"
+        },
+        {
+            "src": "images/artdenim00035.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00035.JPG"
+        },
+        {
+            "src": "images/artdenim00036.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00036.JPG"
+        },
+        {
+            "src": "images/artdenim00037.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00037.JPG"
+        },
+        {
+            "src": "images/artdenim00038.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00038.JPG"
+        },
+        {
+            "src": "images/artdenim00039.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00039.JPG"
+        },
+        {
+            "src": "images/artdenim00040.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00040.JPG"
+        },
+        {
+            "src": "images/artdenim00041.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00041.JPG"
+        },
+        {
+            "src": "images/artdenim00042.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00042.JPG"
+        },
+        {
+            "src": "images/artdenim00043.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00043.JPG"
+        },
+        {
+            "src": "images/artdenim00044.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00044.JPG"
+        },
+        {
+            "src": "images/artdenim00045.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00045.JPG"
+        },
+        {
+            "src": "images/artdenim00046.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00046.JPG"
+        },
+        {
+            "src": "images/artdenim00047.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00047.JPG"
+        },
+        {
+            "src": "images/artdenim00048.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00048.JPG"
+        },
+        {
+            "src": "images/artdenim00049.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00049.JPG"
+        },
+        {
+            "src": "images/artdenim00050.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00050.JPG"
+        },
+        {
+            "src": "images/artdenim00051.JPG",
+            "hotspots": {},
+            "srcLarge": "imageslarge/artdenim00051.JPG"
+        }
+    ],
+    "settings": {
+        "rotation": {
+            "autoRestart": 0,
+            "rotate": "true",
+            "firstImage": 0,
+            "rotationAxis": "vertical",
+            "rotatePeriod": 3,
+            "bounce": false,
+            "rotateDirection": 1,
+            "disabledIfZoom": false
+        },
+        "preloader": {
+            "showLoadedImages": false,
+            "image": "first",
+            "showStartButton": true,
+            "preLoadImages": false
+        },
+        "userInterface": {
+            "showToggleFullscreenButton": true,
+            "showZoombar": true,
+            "showTogglePanButton": "zoom",
+            "showArrows": true,
+            "showZoomButtons": true,
+            "showResetButton": "zoom",
+            "showScreenshotButton": false,
+            "showTogglePlayButton": true,
+            "zoomBarClickable": true,
+            "showToggleRotateButton": "zoom",
+            "defaultCursor": true
+        },
+        "control": {
+            "disableClick": false,
+            "disableDrag": false,
+            "reverseDrag": false,
+            "zoomInStopsRotation": true,
+            "reversePan": false,
+            "disableMouseControl": false,
+            "zoomOutStartsRotation": false,
+            "arrowInterval": 100,
+            "zoomSetsPan": false,
+            "dragSpeed": 0.5,
+            "zoomSteps": 2,
+            "screenshotFullImage": false,
+            "mouseWheelZooms": true,
+            "enableSwing": true,
+            "maxZoom": 800,
+            "playAfterDrag": false,
+            "clickZoomsIn": false,
+            "rotateZoom": false
+        }
+    }
+};
+
+//for maximum compatiblity, assign the data to 1. common named variable, 2. product - specific named variable
+
+var RotationData_3 = productRotationData_1363309381731;
+var RotationData = productRotationData_1363309381731;
